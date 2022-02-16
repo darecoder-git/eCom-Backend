f83f2ac5b33298371699a24775cbc9ca5d78cc76
darecoder-git <purkayasthapravash123@gmail.com>
2022-02-16 18:45:34 +0530

    added files

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose =require("mongoose");
 const dotenv=require("dotenv");
 dotenv.config();
 const useroute=require("./routes/user")
+const authroute=require("./routes/auth")
 
 
 
@@ -16,6 +17,7 @@ process.env.MONGO_URL
 
 app.use(express.json());
 app.use("/api/user",useroute);
+app.use("/api/auth",authroute);
 
 
 app.listen(process.env.PORT||5000 ,()=>{
--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,61 @@
 const router =require("express").Router()
+const user= require("../models/user")
+const CryptoJS=require("crypto-js");
+
+
+
+
 
 //REGISTER
+router.post("/register",async(req,res)=>{
+   const newUser=new user({
+       username:req.body.username,
+       email:req.body.username,
+       password:CryptoJS.AES.encrypt(req.body.password,"process.env.PASS_SEC").toString(),
+   });
+   try{
+   const saveduser= await newuser.save();
+   res.status(201).json(savesduser);
+   }
 
-router.post("/register",(req,res)=>{
-   const newUser=new user
+   
+   catch(err){   
+   res.status(500).json(err);
+   }
+});
+
+
+//LOGIN
+router.post("/login",async(req,res)=>{
+
+   try{
+      
+   
+      const user=await user.findone({username:req.body.user});
+      const hashpassword=
+
+
+
+
+   }
+   catch(err){
+    res.status(500).json(err)
+
+
+
+   }
+
 
 })
 
+
+
+
+
+
+
+
+
+ 
 
 module.exports=router;
